be744eeeb0fe1c2700ae913e39f9685ac99bf414
apathywoke <schizoflow@proton.me>
2024-11-05 08:57:40 +0200

    Have been done home page.

--- src/Pages/Footer/footer.jsx
+++ src/Pages/Footer/footer.jsx
@@ -4,8 +4,8 @@ import style from './footer.module.css'
 const Footer = () => {
     return(
         <footer className={`${style.footer_class} ${style.fonts}`}>
-            <span>Website by student</span>
-            <span>Denys Shvets</span>
+            <span>Website created by</span>
+            <span>student Denys Shvets</span>
         </footer>
     )
 }
--- src/Pages/Main/main.jsx
+++ src/Pages/Main/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import style from './main.module.css'
 import Home from '../Home/home'
 import Header from '../Header/header'
 import Footer from '../Footer/footer'
