d568f367ce0483a411c4dedad47b5f4c23112d83
apathywoke <schizoflow@proton.me>
2024-11-05 07:13:31 +0200

    Initial.

---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './home.module.css'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    return(
+        
+    )
+}
+
+export default Home;
